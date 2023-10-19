43437705c672e46f73864ca4b6dff389ea717bb9
Gitarackur <peterosah5@gmail.com>
2023-10-20 00:33:52 +0100

    chore: added jsDOC to HapiJS plugin

--- plugins/hapi/index.ts
+++ plugins/hapi/index.ts
@@ -2,30 +2,48 @@ import * as hapi from "hapi";
 import { version as treblleVersion } from "../../package.json";
 import TreblleCore, { TrebllePluginPayload, TreblleConfig } from "../../core";
 
+/**
+ * A class to integrate Treblle with a Hapi.js application.
+ */
 export default class TreblleHapi {
-  private static handler: TreblleCore;
+  /**
+   * The TreblleCore instance used for monitoring and logging.
+   * @type {TreblleCore}
+   * @private
+   */
+  private static treblleCore: TreblleCore;
 
-  static setup(cfg: TreblleConfig) {
-    TreblleHapi.handler = new TreblleCore({
-      ...cfg,
+  /**
+   * Set up the TreblleCore instance with the provided configuration.
+   * @param {TreblleConfig} config - The Treblle configuration object.
+   */
+  static setup(config: TreblleConfig) {
+    TreblleHapi.treblleCore = new TreblleCore({
+      ...config,
     });
   }
 
-  static plugin(cfg: TreblleConfig): hapi.Plugin<TreblleConfig> {
-    if (!(TreblleHapi.handler instanceof TreblleCore)) {
-      TreblleHapi.setup(cfg);
+  /**
+   * Create a Hapi.js plugin that acts as a Treblle integration.
+   * This plugin collects and logs request and response data.
+   * @param {TreblleConfig} config - The Treblle configuration object.
+   * @returns {hapi.Plugin<TreblleConfig>} - Hapi.js plugin object.
+   */
+  static plugin(config: TreblleConfig): hapi.Plugin<TreblleConfig> {
+    if (!(TreblleHapi.treblleCore instanceof TreblleCore)) {
+      TreblleHapi.setup(config);
     }
 
     return {
       name: "TreblleHapi",
       version: treblleVersion,
       register(server, options) {
-        if (!(TreblleHapi.handler instanceof TreblleCore)) {
+        if (!(TreblleHapi.treblleCore instanceof TreblleCore)) {
           TreblleHapi.setup(options);
         }
         server.ext("onPreResponse", function (request, h) {
           const response = request.response as any;
-          const Handler = TreblleHapi.handler;
+          const Handler = TreblleHapi.treblleCore;
 
           let responseData = '';
           response.events.on('peek', (chunk: string) => {
@@ -58,6 +76,12 @@ export default class TreblleHapi {
     }
   };
 
+  /**
+   * Extract relevant data from a Hapi.js Request object for the request.
+   * @param {hapi.Request & { getIp(): string }} req - The Hapi Request object with the `getIp` method.
+   * @returns {TrebllePluginPayload['request']} - The request data as a TrebllePluginPayload object.
+   * @private
+   */
   static extractRequestData(req: hapi.Request & { getIp(): string }): TrebllePluginPayload['request'] {
     return {
       ip: req.getIp(),
@@ -69,6 +93,12 @@ export default class TreblleHapi {
     }
   }
 
+  /**
+   * Extract relevant data from a Hapi.js Request object for the response.
+   * @param {hapi.Request["response"]} res - The Hapi Request's response object.
+   * @returns {TrebllePluginPayload['response']} - The response data as a TrebllePluginPayload object.
+   * @private
+   */
   private static extractResponseData(res: hapi.Request["response"]): TrebllePluginPayload['response'] {
     console.log(res);
 
@@ -80,6 +110,12 @@ export default class TreblleHapi {
     }
   }
 
+  /**
+   * Extract server-related data from a Hapi.js Request object.
+   * @param {hapi.Request & { getIp(): string }} req - The Hapi Request object with the `getIp` method.
+   * @returns {Object} - Server data with IP and protocol information.
+   * @private
+   */
   private static extractServerData(req: hapi.Request & { getIp(): string }) {
     return {
       ip: req.getIp(),
